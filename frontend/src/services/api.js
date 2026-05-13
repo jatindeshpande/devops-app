73e02b65edb4783503eb76a4edbc10d48e68c887
jatindeshpande <jatinvd1997@gmail.com>
2026-05-13 20:20:59 +0530

    update getMainApi function name

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 const API_URL = import.meta.env.VITE_API_URL
 
-export async function getMainAPI(params) {
+export async function getMainApi(params) {
     const response = await fetch(`${API_URL}/`)
     return response.json()
 }
